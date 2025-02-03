7205506b3e5e1f3814db039be18e9e0094e07af3
Jacob Costen <32058792+oculometric@users.noreply.github.com>
2025-02-03 09:32:08 +0000

    Create buffer.h

---
+++ src/buffer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include<vulkan/vulkan.h>
+
+class PTBuffer
+{
+private:
+    VkBuffer buffer;
+    // memory block
+    // mapped memory
+    // size
+    
+public:
+    // map/unmap memory, get mapped address
+    // get size
+    // get buffer
+    // creation and destruction
+    // copying from one to another?
+};
