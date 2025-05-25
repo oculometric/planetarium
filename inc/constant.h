1a8dc8ecf076bdb065ccc4e44341c108282b63c7
Jacob Costen <32058792+oculometric@users.noreply.github.com>
2025-05-25 10:47:38 +0100

    command output now actually works!

--- inc/constant.h
+++ inc/constant.h
@@ -42,18 +42,33 @@ struct SceneUniforms
 
 #include <fstream>
 #include <string>
+#include <array>
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#define popen _popen
+#define pclose _pclose
+#endif
+
 
 inline int exec(std::string command, std::string& output)
 {
-    int status = std::system((command + " > tmp.txt").c_str());
-    output = "";
-    
-    std::ifstream file("tmp.txt", std::ios::ate);
-    size_t size = file.tellg();
-    output.resize(size, ' ');
-    file.seekg(0);
-    file.read(output.data(), size);
-    file.close();
+    const size_t buffer_size = 512;
+    std::array<char, buffer_size> buffer;
 
-    return status;
+    auto pipe = popen((command + " 2>&1").c_str(), "r");
+    if (!pipe)
+    {
+        output = "popen failed.";
+        return -1;
+    }
+
+    output = "";
+    size_t count;
+    do {
+        if ((count = fread(buffer.data(), 1, buffer_size, pipe)) > 0)
+            output.insert(output.end(), std::begin(buffer), std::next(std::begin(buffer), count));
+    } while (count > 0);
+
+    return pclose(pipe);
 }
